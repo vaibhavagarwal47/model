5ebd1c7eb06c861bf6bdcebc68e194d353debeba
Vaibhav Agarwal <dpsvai8147@gmail.com>
2023-02-14 12:11:49 +0530

    update files

--- script.js
+++ script.js
@@ -10,4 +10,34 @@ const onProgress = (event) => {
     progressBar.classList.remove("hide");
   }
 };
-document.querySelector("model-viewer").addEventListener("progress", onProgress);
+const modelViewer = document.querySelector("model-viewer")
+modelViewer.addEventListener("progress", onProgress);
+const apuButton = modelViewer.querySelector('button[slot="hotspot-3"]');
+const apuLabel =  document.querySelector('.APULabel');
+
+const connectButton = modelViewer.querySelector('button[slot="hotspot-4"]');
+const connectLabel =  document.querySelector('.ConnectLabel');
+
+const fuelButton = modelViewer.querySelector('button[slot="hotspot-5"]');
+const fuelLabel =  document.querySelector('.FUELLabel');
+
+apuButton.addEventListener('click', () => {
+  let visibility = apuLabel.style.visibility;
+  visibility = visibility === 'visible' ? 'hidden' : 'visible';
+  apuLabel.style.visibility = visibility;
+  console.log(apuButton.childNodes[1])
+});
+
+connectButton.addEventListener('click', () => {
+  let visibility = connectLabel.style.visibility;
+  visibility = visibility === 'visible' ? 'hidden' : 'visible';
+  connectLabel.style.visibility = visibility;
+  console.log(connectButton.childNodes[1])
+});
+
+fuelButton.addEventListener('click', () => {
+  let visibility = fuelLabel.style.visibility;
+  visibility = visibility === 'visible' ? 'hidden' : 'visible';
+  fuelLabel.style.visibility = visibility;
+  console.log(fuelButton.childNodes[1])
+});
